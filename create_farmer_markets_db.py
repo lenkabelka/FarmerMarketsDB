e7dc6717b367c890c8b5dbf6e3eecc53417bade7
lenkabelka <belgradskaiaelena@gmail.com>
2025-03-09 11:10:28 +0100

    Uncomment the functions for DB creation

--- create_farmer_markets_db.py
+++ create_farmer_markets_db.py
@@ -1,7 +1,7 @@
 import database_creator as creator
 import database_from_csv_fill as fill
 
-#creator.create_database()
-#creator.create_tables_in_database()
+creator.create_database()
+creator.create_tables_in_database()
 
 fill.fill_database_from_csv()
